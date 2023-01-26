f8fa8ed6c35679e33949e8e5916d94e526546a75
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-01-26 13:03:15 +0100

    Lektion 2 (Design av Fixxo.)

--- Lektion-2/webapp/src/components/individuals/MenuLinkIcons.js
+++ Lektion-2/webapp/src/components/individuals/MenuLinkIcons.js
@@ -4,10 +4,10 @@ import MenuLink from './MenuLink'
 const MenuLinkIcons = () => {
   return (
     <div className="menulinkicons">
-      <MenuLink name="H" link="/cart" />
-      <MenuLink name="H" link="/cart" />
-      <MenuLink name="H" link="/cart" />
-      <MenuLink name="H" link="/cart" />
+      <MenuLink name={(<i className="fa-regular fa-magnifying-glass"></i>)}  link="/search" />
+      <MenuLink name={(<i className="fa-regular fa-code-compare"></i>)}  link="/compare" />
+      <MenuLink name={(<i className="fa-regular fa-heart"></i>)}  link="/wishlist" />
+      <MenuLink name={(<i className="fa-regular fa-bag-shopping"></i>)} link="/cart" />
     </div>
   )
 }
