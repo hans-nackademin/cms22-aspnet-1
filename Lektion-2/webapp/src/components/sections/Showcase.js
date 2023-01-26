f709498251b0a2910fb8a0df9381c5047375b50d
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-01-26 14:22:52 +0100

    Lektion 2 (Design av Fixxo.)

---
+++ Lektion-2/webapp/src/components/sections/Showcase.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from '../../assets/images/showcase-img.png'
+
+const Showcase = () => {
+  return (
+    <section className="showcase">
+        
+        <div className="container">
+            <div className="content">a</div>
+            <img src={Image} alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default Showcase
--- Lektion-2/webapp/src/views/HomeView.js
+++ Lektion-2/webapp/src/views/HomeView.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/sections/Header'
+import Showcase from '../components/sections/Showcase'
 
 
 const HomeView = () => {
   return (
     <div id="home">
       <Header />
+      <Showcase />
     </div>
   )
 }
