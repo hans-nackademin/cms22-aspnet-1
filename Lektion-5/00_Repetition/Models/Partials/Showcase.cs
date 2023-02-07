f9e404e1aea655803d612f2e6b3406c5e1197dda
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-02-07 10:28:10 +0100

    Lektion 5 (Services in ASP.NET MVC)

---
+++ Lektion-5/00_Repetition/Models/Partials/Showcase.cs
@@ -0,0 +1,11 @@
+﻿namespace _00_Repetition.Models.Partials
+{
+    public class Showcase
+    {
+        public string Title_1 { get; set; } = string.Empty;
+        public string Title_2 { get; set; } = string.Empty;
+        public string Title_3 { get; set; } = string.Empty;
+        public string ButtonText { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
