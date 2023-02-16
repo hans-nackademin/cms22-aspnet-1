785f5ddc4b24647b0776b01f568c2c0bf42e4078
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-02-16 16:15:57 +0100

    Lektion 8 (Identity, Roles, Policy, Claims)

--- Lektion-8/IdentityExample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityExample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
